5cfac33038fce844072b88f410d64b806848228f
Michael Douglas <mikedgls17@gmail.com>
2022-11-17 05:42:26 -0300

    projeto criado

---
+++ src/test/java/io/github/mikedgl/WorldCupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mikedgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldCupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
